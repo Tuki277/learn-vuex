b5067d52bfa14f2488009220b10ae3fb2757c381
Tran Hoang Huy <huybeos2707@gmail.com>
2021-04-26 22:58:49 +0700

    getter

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -3,15 +3,10 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
     name: 'ProgressBar',
-    computed: {
-        progress () {
-            return Math.round(
-                (this.$store.state.todos.filter(todo => todo.completed).length /
-            this.$store.state.todos.length ) * 100 )
-        }
-    }
+    computed: mapGetters(['progress'])
 }
 </script>
 
